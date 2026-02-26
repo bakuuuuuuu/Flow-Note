6794c250359798a66070b4122d40bae3a3311f9a
bakuuuuuuu <dudwls5674@naver.com>
2026-02-26 21:53:08 +0900

    feat: 리스트 수정 및 카드 연쇄 삭제(Cascade Delete) 기능 구현

--- server/routes/listRoutes.js
+++ server/routes/listRoutes.js
@@ -6,4 +6,10 @@ const { protect } = require('../middleware/authMiddleware');
 // [POST] 리스트 생성
 router.post('/', protect, listController.createList);
 
+// [PATCH] 리스트 수정
+router.patch('/:id', protect, listController.updateList);
+
+// [DELETE] 리스트 삭제
+router.delete('/:id', protect, listController.deleteList);
+
 module.exports = router;
